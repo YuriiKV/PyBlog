a7ae9dcd73908975cc1b52bb36c48c0a8440b853
Jura <jurakulek@gmail.com>
2018-10-05 18:51:10 +0300

    add db migration

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'falled',
-        'title': 'Blog post 1',
-        'content': 'First post content',
-        'date_posted': 'October 5, 2018'
-    },
-     {
-        'author': 'Yurii Kulyk',
-        'title': 'Blog post 2',
-        'content': 'Second post content',
-        'date_posted': 'October 6, 2018'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
