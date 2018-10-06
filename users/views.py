3079daf92fe85058d1369546af94d1fc9059d864
Jura <jurakulek@gmail.com>
2018-10-06 11:25:54 +0300

    add login and logout pages

--- users/views.py
+++ users/views.py
@@ -8,8 +8,12 @@ def register(request):
 		if form.is_valid():  #check if the form is valid
 			form.save()
 			username = form.cleaned_data.get('username')
-			messages.success(request, 'Account creater for {}!'.format(username))
-			return redirect('blog-home')
+			messages.success(request, 'Your account has been created! You are now able to log in'.format(username))
+			return redirect('login')
 	else:
 		form = UserRegisterForm()
-	return render(request, 'users/register.html', {'form': form})
+	return render(request, 'users/register.html', {'form': form})  #in render fild first param always must be request
+
+
+def profile(request):
+	return render(request, 'users/profile.html')
